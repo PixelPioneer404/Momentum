778de33b4b72ae3b96b904f62d5267fa215656d7
PixelPioneer404 <rajbeersaha.me@gmail.com>
2025-09-24 18:58:36 +0530

    created the login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,45 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { StatusBar } from "react-native";
 import "./global.css";
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    "BobobyGroovy": require("../assets/fonts/bobogy-groofy.otf"),
+    "AlanSansMedium": require("../assets/fonts/AlanSans-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack>
+    </>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,43 @@
-import { Text, View } from "react-native";
- 
+import LottieView from "lottie-react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import GoogleIcon from "../assets/icons/google-icon.svg";
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View className="flex-1 items-center bg-[#ccd5ae]">
+      <View className="items-center mt-[6rem]">
+        <LottieView
+          source={require("../assets/lottie/TaskLottieLogin.json")}
+          style={{ width: 380, height: 380 }}
+          autoPlay
+          loop
+          speed={1.2}
+        />
+      </View>
+      <View className="justify-center mt-6 flex-col items-center text-center">
+        <Text className="text-[1.8rem] font-alan-sans-medium text-[#191923] text-center">Welcome to</Text>
+        <Text className="text-[5rem] font-bobogy text-[#191923] text-center mt-[-15px]">Momentum</Text>
+      </View>
+      <View className="justify-center items-center mt-[120px]">
+        <TouchableOpacity 
+          className="w-[70vw] h-[66px] bg-[#283618] rounded-3xl"
+          style={{
+            shadowColor: '#000000',
+            shadowOffset: {
+              width: 0,
+              height: 6,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 10,
+            elevation: 8, // For Android
+          }}
+        >
+          <View className="flex-1 flex-row justify-center items-center gap-3">
+            <GoogleIcon width={24} height={24} />
+            <Text className="text-[#ccd5ae] font-alan-sans-medium text-[1.4rem]">Log in with Google</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
